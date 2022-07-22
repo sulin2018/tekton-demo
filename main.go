f9649e4faf684f9aa5bd0f4943138b1bd5bf7972
lin.su <lin.su@za.group>
2022-07-22 18:25:12 +0800

    s

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong, hello world!",
+			"message": "pong, hello world! ZA!",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
